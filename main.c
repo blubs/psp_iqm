314255127d7d32d4b5efd2d40c9dd416b2a02394
blubs <lg@luisg.net>
2023-09-06 01:54:30 -0700

    Add test sceGU drawing Add debug prints drawing Add IQM / PNG assets

--- main.c
+++ main.c
@@ -22,9 +22,69 @@ PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
 
 #define printf pspDebugScreenPrintf
 
+
+#define BUF_WIDTH (512)
+#define SCR_WIDTH (480)
+#define SCR_HEIGHT (272)
+static unsigned int __attribute__((aligned(16))) display_list[262144];
+
+
+void *draw_buffer;
+void *display_buffer;
+void *depth_buffer;
+
+
+void init_gu() {
+    sceGuInit();
+    sceGuStart(GU_DIRECT, display_list);
+
+    // Setup buffers
+    draw_buffer = getStaticVramBuffer(BUF_WIDTH, SCR_HEIGHT, GU_PSM_8888);
+    display_buffer = getStaticVramBuffer(BUF_WIDTH, SCR_HEIGHT, GU_PSM_8888);
+    depth_buffer = getStaticVramBuffer(BUF_WIDTH, SCR_HEIGHT, GU_PSM_4444);
+    sceGuDrawBuffer(GU_PSM_8888, draw_buffer, BUF_WIDTH);
+    sceGuDispBuffer(SCR_WIDTH, SCR_HEIGHT, display_buffer, BUF_WIDTH);
+    sceGuDepthBuffer(depth_buffer, BUF_WIDTH);
+
+    // Center the region where drawing is performed in the virtual coord space
+    unsigned int virtual_coordinate_space_width = 4096;
+    unsigned int virtual_coordinate_space_height = 4096;
+    // X-coord of top-left corner
+    unsigned int psp_screen_tl_x = (virtual_coordinate_space_width/2) - (SCR_WIDTH/2);
+    // Y-coord of top-left corner 
+    unsigned int psp_screen_tl_y = (virtual_coordinate_space_height/2) - (SCR_HEIGHT/2);
+    sceGuOffset(psp_screen_tl_x, psp_screen_tl_y);
+
+    // Center window-size viewport  in virtual coord space
+    unsigned int center_x = virtual_coordinate_space_width/2;
+    unsigned int center_y = virtual_coordinate_space_height/2;
+    sceGuViewport(center_x, center_y, SCR_WIDTH, SCR_HEIGHT);
+
+    // Set near and far range values for depth buffer
+    sceGuDepthRange((1<<16) - 1, 0);
+    // Depth buffer is inverted (a closer than b --> a > b)
+    sceGuDepthFunc(GU_GEQUAL);
+    // Specify region to scissor
+    sceGuScissor(0,0,SCR_WIDTH,SCR_HEIGHT);
+    sceGuEnable(GU_SCISSOR_TEST);
+    sceGuFrontFace(GU_CW);
+    sceGuShadeModel(GU_SMOOTH);
+    sceGuEnable(GU_CULL_FACE);
+    sceGuEnable(GU_TEXTURE_2D);
+    sceGuEnable(GU_CLIP_PLANES);
+    sceGuFinish();
+    // Wait for draw commands to finish processing
+    sceGuSync(GU_SYNC_WHAT_DONE,0);
+    // Wait for vertical blank start
+    sceDisplayWaitVblankStart();
+    // Turn display on
+    sceGuDisplay(GU_TRUE);
+}
+
+
 int main(int argc, char *argv[]) {
     setupCallbacks();
-
+    init_gu();
     pspDebugScreenInit();
 
     FILE *f = fopen("test.txt", "r");
@@ -42,10 +102,37 @@ int main(int argc, char *argv[]) {
         pspDebugScreenPrintf("Read text: \"%s\"\n", text);
     }
 
+    void *cur_draw_buffer = draw_buffer;
     fclose(f);
+    unsigned int frame = 0;
     while(running()) {
+        sceGuStart(GU_DIRECT, display_list);
+        // Smoothly fade between 0 and 1:
+        float fade = 0.5f * (sin((float)frame / 10.0f) + 1.0f);
+        sceGuClearColor((int)(0x0000ff * fade));
+        sceGuClearDepth(0);
+        // sceGuClear(GU_DEPTH_BUFFER_BIT);
+        // sceGuClear(GU_COLOR_BUFFER_BIT);
+        sceGuClear(GU_COLOR_BUFFER_BIT | GU_DEPTH_BUFFER_BIT);
+        sceGuFinish();
+
+
+        // Draw debug screen on top of the current draw buffer
+        // Set location in VRAM to draw debugscreen to:
+        pspDebugScreenSetOffset((int) cur_draw_buffer);
+        sceGuSync(GU_SYNC_FINISH,GU_SYNC_WHAT_DONE);
+        // pspDebugScreenSetOffset((int) display_buffer);
+        pspDebugScreenSetXY(40,3);
+        pspDebugScreenPrintf("Reading file: \"test.txt\"");
+        pspDebugScreenSetXY(40,4);
+        pspDebugScreenPrintf("Frame: \"%d\"", frame);
+        sceDisplayWaitVblankStart();
+        cur_draw_buffer = sceGuSwapBuffers();
+        sceGuSync(GU_SYNC_FINISH,GU_SYNC_WHAT_DONE);
+        frame += 1;
     }
 
-
+    sceGuTerm();
+    sceKernelExitGame();
     return 0;
 }
